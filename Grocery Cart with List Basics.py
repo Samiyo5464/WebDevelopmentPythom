809043edb8aa9d2a290113283cf1e992f29b3762
Sanjog Mall <65735290+SanjogMall@users.noreply.github.com>
2021-01-14 16:43:16 -0800

    Add files via upload

---
+++ Grocery Cart with List Basics.py
@@ -0,0 +1,14 @@
+print("Welcome to the Grocery Store!")
+list_items = []
+while True:
+    new_item = input("Add items into the grocery list? Type 'Q' to quit: ")
+    if new_item == "q":
+        print("thank you for using my grocery\nYour final grocery list is as follows:")
+        break
+    else:
+        list_items.append(new_item)
+        print(list_items)
+
+
+for item in sorted(list_items):
+    print("-"+item)
